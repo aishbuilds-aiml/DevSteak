fdabe8c3f9637dc91f89612a2d58d0c57e18e0a2
Aish <fairyze15@gmail.com>
2026-05-18 03:55:10 +0530

    Built premium DevStreak UI

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,12 @@ class DevStreakApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'DevStreak',
-      theme: ThemeData.dark(),
+
+      theme: ThemeData(
+        brightness: Brightness.dark,
+        scaffoldBackgroundColor: const Color(0xFF070B1A),
+      ),
+
       home: const HomeScreen(),
     );
   }
@@ -26,164 +31,459 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int streak = 7;
-  int xp = 320;
-  double progress = 0.7;
 
-  void codedToday() {
-  setState(() {
-    streak++;
-    xp += 10;
-
-    if (progress < 1.0) {
-      progress += 0.1;
-    }
-  });
-}
+  int streak = 43;
+  int xp = 250;
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-      backgroundColor: const Color(0xFF0F172A),
-      appBar: AppBar(
-        title: const Text("DevStreak 🔥"),
-        backgroundColor: Colors.transparent,
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const SizedBox(height: 20),
 
-            Container(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: const Color(0xFF1E293B),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "Current Streak",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text(
-                    "$streak Days 🔥",
-                    style: const TextStyle(
-                      fontSize: 22,
-                      color: Colors.greenAccent,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+      body: SafeArea(
 
-            const SizedBox(height: 20),
+        child: SingleChildScrollView(
 
-            Container(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: const Color(0xFF1E293B),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  const Text(
-                    "XP Points",
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  Text(
-                    "$xp XP ⭐",
-                    style: const TextStyle(
-                      fontSize: 22,
-                      color: Colors.amber,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                ],
-              ),
-            ),
+          child: Padding(
+            padding: const EdgeInsets.all(20),
 
-            Container(
-              padding: const EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                color: const Color(0xFF1E293B),
-                borderRadius: BorderRadius.circular(20),
-              ),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "Today's Goal",
-                    style: TextStyle(fontSize: 20),
-                  ),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
 
-                  const SizedBox(height: 15),
+              children: [
 
-                  LinearProgressIndicator(
-                    value: progress,
-                    minHeight: 12,
-                    borderRadius: BorderRadius.circular(20),
-                    backgroundColor: Colors.white12,
-                    valueColor: const AlwaysStoppedAnimation(
-                      Colors.greenAccent,
-                    ),
-                  ),
+                const SizedBox(height: 20),
 
-                  const SizedBox(height: 10),
-
-                  Text(
-                    "${(progress * 100).toInt()}% Completed",
-                    style: const TextStyle(
-                      color: Colors.white70,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-
-            const SizedBox(height: 40),
-
-            SizedBox(
-              width: double.infinity,
-              height: 60,
-              child: ElevatedButton(
-                onPressed: codedToday,
-                style: ElevatedButton.styleFrom(
-                  backgroundColor: Colors.greenAccent,
-                  foregroundColor: Colors.black,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                ),
-                child: const Text(
-                  "I CODED TODAY 🚀",
+                const Text(
+                  "Hey, Aish 👋",
+                  
                   style: TextStyle(
-                    fontSize: 18,
+                    fontSize: 32,
                     fontWeight: FontWeight.bold,
+                    color: Colors.white,
                   ),
                 ),
-              ),
+
+                const SizedBox(height: 30),
+
+                // TOP CARD
+
+                Container(
+                  padding: const EdgeInsets.all(24),
+
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(30),
+
+                    gradient: const LinearGradient(
+                      begin: Alignment.topLeft,
+                      end: Alignment.bottomRight,
+
+                      colors: [
+                        Color(0xFF1B2344),
+                        Color(0xFF0D1224),
+                      ],
+                    ),
+
+                    boxShadow: [
+                      BoxShadow(
+                        color: Colors.purple.withOpacity(0.3),
+                        blurRadius: 25,
+                        spreadRadius: 1,
+                      ),
+                    ],
+                  ),
+
+                  child: Column(
+                    children: [
+
+                      Row(
+                        mainAxisAlignment:
+                        MainAxisAlignment.spaceBetween,
+
+                        children: [
+
+                          Column(
+                            crossAxisAlignment:
+                            CrossAxisAlignment.start,
+
+                            children: [
+
+                              Text(
+                                "$streak day streak",
+
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+
+                              const SizedBox(height: 10),
+
+                              Text(
+                                "XP today",
+
+                                style: TextStyle(
+                                  color:
+                                  Colors.white.withOpacity(0.7),
+                                ),
+                              ),
+
+                              const SizedBox(height: 10),
+
+                              Text(
+                                "+$xp XP",
+
+                                style: const TextStyle(
+                                  color: Colors.greenAccent,
+                                  fontSize: 26,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+
+                          Container(
+                            width: 120,
+                            height: 120,
+
+                            decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+
+                              border: Border.all(
+                                color: Colors.greenAccent,
+                                width: 10,
+                              ),
+
+                              boxShadow: [
+                                BoxShadow(
+                                  color:
+                                  Colors.greenAccent.withOpacity(0.5),
+                                  blurRadius: 20,
+                                ),
+                              ],
+                            ),
+
+                            child: const Center(
+                              child: Text(
+                                "100%",
+
+                                style: TextStyle(
+                                  fontSize: 28,
+                                  fontWeight: FontWeight.bold,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      const SizedBox(height: 30),
+
+                      Container(
+                        padding: const EdgeInsets.symmetric(
+                          vertical: 18,
+                        ),
+
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+
+                          gradient: const LinearGradient(
+                            colors: [
+                              Colors.orange,
+                              Colors.deepOrange,
+                            ],
+                          ),
+                        ),
+
+                        child: const Center(
+                          child: Text(
+                            "Day Complete! You're on fire 🔥",
+
+                            style: TextStyle(
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 30),
+
+                // ACTION BUTTONS
+
+                Row(
+                  mainAxisAlignment:
+                  MainAxisAlignment.spaceBetween,
+
+                  children: [
+
+                    buildActionButton(
+                      Icons.code,
+                      "Code",
+                    ),
+
+                    buildActionButton(
+                      Icons.menu_book,
+                      "Learn",
+                    ),
+
+                    buildActionButton(
+                      Icons.coffee,
+                      "Break",
+                    ),
+
+                    buildActionButton(
+                      Icons.bolt,
+                      "Boost",
+                    ),
+                  ],
+                ),
+
+                const SizedBox(height: 40),
+
+                // WEEK SECTION
+
+                Container(
+                  height: 220,
+                  width: double.infinity,
+
+                  padding: const EdgeInsets.all(24),
+
+                  decoration: BoxDecoration(
+                    color: const Color(0xFF111936),
+
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+
+                  child: Column(
+                    crossAxisAlignment:
+                    CrossAxisAlignment.start,
+
+                    children: [
+
+                      Row(
+                        mainAxisAlignment:
+                        MainAxisAlignment.spaceBetween,
+
+                        children: [
+
+                          const Text(
+                            "This week",
+
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+
+                          Text(
+                            "+24% vs last week",
+
+                            style: TextStyle(
+                              color: Colors.greenAccent.shade400,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+
+                      const Spacer(),
+
+                      Row(
+                        mainAxisAlignment:
+                        MainAxisAlignment.spaceAround,
+
+                        children: const [
+
+                          Text("M"),
+                          Text("T"),
+                          Text("W"),
+                          Text("T"),
+                          Text("F"),
+                          Text("S"),
+                          Text("S"),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+
+                const SizedBox(height: 100),
+              ],
+            ),
+          ),
+        ),
+      ),
+
+      // FLOATING BUTTON
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.deepPurple,
+
+        onPressed: () {
+
+          setState(() {
+            xp += 10;
+            streak += 1;
+          });
+        },
+
+        child: const Icon(Icons.add),
+      ),
+
+      // BOTTOM NAVIGATION
+
+      bottomNavigationBar: Container(
+        margin: const EdgeInsets.all(16),
+
+        padding: const EdgeInsets.symmetric(
+          vertical: 10,
+        ),
+
+        decoration: BoxDecoration(
+          color: const Color(0xFF111936),
+
+          borderRadius: BorderRadius.circular(30),
+        ),
+
+        child: const Row(
+          mainAxisAlignment:
+          MainAxisAlignment.spaceAround,
+
+          children: [
+
+            BottomNavItem(
+              icon: Icons.home,
+              label: "Home",
+              active: true,
             ),
 
-            const SizedBox(height: 30),
+            BottomNavItem(
+              icon: Icons.bar_chart,
+              label: "Stats",
+            ),
 
-            const Center(
-              child: Text(
-                "Consistency beats motivation 💡",
-                style: TextStyle(
-                  fontSize: 18,
-                  color: Colors.white70,
-                ),
-              ),
+            BottomNavItem(
+              icon: Icons.emoji_events,
+              label: "Awards",
+            ),
+
+            BottomNavItem(
+              icon: Icons.notifications,
+              label: "Inbox",
+            ),
+
+            BottomNavItem(
+              icon: Icons.person,
+              label: "Profile",
             ),
           ],
         ),
       ),
+    );
+  }
+}
+
+Widget buildActionButton(
+    IconData icon,
+    String title,
+    ) {
+
+  return Container(
+    width: 75,
+    height: 90,
+
+    decoration: BoxDecoration(
+      color: const Color(0xFF111936),
+
+      borderRadius: BorderRadius.circular(24),
+
+      border: Border.all(
+        color: Colors.white.withOpacity(0.05),
+      ),
+    ),
+
+    child: Column(
+      mainAxisAlignment:
+      MainAxisAlignment.center,
+
+      children: [
+
+        CircleAvatar(
+          radius: 20,
+          backgroundColor: Colors.deepPurple,
+
+          child: Icon(
+            icon,
+            color: Colors.white,
+          ),
+        ),
+
+        const SizedBox(height: 10),
+
+        Text(
+          title,
+
+          style: const TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    ),
+  );
+}
+
+class BottomNavItem extends StatelessWidget {
+
+  final IconData icon;
+  final String label;
+  final bool active;
+
+  const BottomNavItem({
+    super.key,
+    required this.icon,
+    required this.label,
+    this.active = false,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+
+      children: [
+
+        Icon(
+          icon,
+          color:
+          active
+              ? Colors.deepPurpleAccent
+              : Colors.white54,
+        ),
+
+        const SizedBox(height: 5),
+
+        Text(
+          label,
+
+          style: TextStyle(
+            color:
+            active
+                ? Colors.white
+                : Colors.white54,
+
+            fontSize: 12,
+          ),
+        ),
+      ],
     );
   }
 }
